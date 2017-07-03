902cc253be962b725c7bfadaede081dd146d3261
Dale Pollitt <dale.negativezero@gmail.com>
2017-07-03 16:24:04 -0700

    Created correctness test

--- SubstringSearch.rb
+++ SubstringSearch.rb
@@ -26,6 +26,7 @@ module SubstringSearch
           return i if (j + 1) == m
         end
       end
+      # no match found, return string length
       n
     end
   end
@@ -37,14 +38,19 @@ module SubstringSearch
     def initialize(pat)
       @pat = pat.split('')
       m = pat.length
+      # initialize our DFA with 0 values our
+      # our DFA is represented as a 2 dimensional array
+      # we will provide an index for a possible state for each ASCII value
       @dfa = Array.new(ASCII_CHARS) { Array.new(m, 0) }
       @dfa[@pat[0].ord][0] = 1
-
-      #j = 1
       x = 0
+      # Computes the DFA
       (1..m-1).each do |j|
+        # Create mismatch cases
         (0..ASCII_CHARS - 1).each { |c| @dfa[c][j] = @dfa[c][x] }
+        # create the match cases
         @dfa[@pat[j].ord][j] = j + 1
+        # update restart case
         x = @dfa[@pat[j].ord][x]
       end
     end
@@ -56,39 +62,57 @@ module SubstringSearch
       m = @pat.length
       txt_array = txt.split('')
 
+      # Progress through the sates of our DFA
       while i < n && j < m
-        # check for nil
         val = @dfa[txt_array[i].ord][j]
         val.nil? ? j = 0 : j = val
         i += 1
       end
+
       if j == m
+        # return our index if a match
         i - m
       else
+        # no match found, return string length
         n
       end
+
     end
   end
 
+  # Boyer-Moore Substring Search Algorithm.  This algorithm creates a
+  # skip table which represents how many values we can skip each time
+  # on a mismatch. Each search begins from right to left and has potential to
+  # skip m values on a miss which gives a best case runtime of m/n.  Our worst
+  # case runtime is nm however, as we have potential to degrade to brute for on misses
+  # at the end of each substring search (miss on first character of pattern)
   class BoyerMoore
+
     def initialize(pat)
       @pat = pat.split('')
       m = pat.length
       r = ASCII_CHARS
       @right = Array.new(r)
 
-      (0..r-1).each {|c| @right[c] = -1 }
-      (0..m-1).each {|j| @right[@pat[j].ord] = j}
+      # create the skip table
+      # characters not in the pattern are initialized to -1
+      # rightmost character positions are computed for chars in pattern
+      (0..r-1).each { |c| @right[c] = -1 }
+      (0..m-1).each { |j| @right[@pat[j].ord] = j }
     end
+
     def search(txt)
       txt_array = txt.split('')
       n = txt.length
       m = @pat.length
       i = 0
+      # search for pattern
       while i <= n - m
         skip = 0
         j = m - 1
 
+        # check for pattern match at current index
+        # and calculate or skip value if no match found
         while j >= 0
           if @pat[j] != txt_array[i + j]
             skip = j - @right[txt_array[i + j].ord]
@@ -99,9 +123,11 @@ module SubstringSearch
           end
           j -= 1
         end
+        # return our index if we did not skip
         return i if skip.zero?
         i += skip
       end
+      # no match found
       n
     end
   end
--- substring_search_test.rb
+++ substring_search_test.rb
@@ -1,22 +1,148 @@
 require 'hitimes'
 require "./SubstringSearch.rb"
 
-rand = Random.new_seed
-vals = ['a', 't', 'g']
-txt = ''
 
-10.times do
-  #num = rand(3)
-  txt << "AAAAA"
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()_=-+?><.,"
+alphabet = alphabet.split('')
+alphabet.push("NEEDLE")
+
+def correctness(alphabet)
+
+  txt = ''
+  needle = 'NEEDLE'
+  fail = 0
+  pass = 0
+  found = 0
+  brute_force_search = SubstringSearch::BruteForce.new(needle)
+  kmp_search = SubstringSearch::KMP.new(needle)
+  boyer_moore_search = SubstringSearch::BoyerMoore.new(needle)
+
+
+  # Random Cases, run 1k times
+  10_000.times do
+    txt = ''
+    # make a random string of 100 characters
+    100.times do
+      num = rand(alphabet.length)
+      txt << alphabet[num]
+    end
+
+    brute_force = brute_force_search.search(txt)
+    kmp = kmp_search.search(txt)
+    boyer_moore = boyer_moore_search.search(txt)
+
+    if brute_force == kmp && brute_force == boyer_moore
+      pass += 1
+    else
+      fail += 1
+    end
+
+    if brute_force < txt.length
+      found += 1
+    end
+
+  end
+
+  puts "========================RANDOM CASES=============================\n"
+  puts "Pass cases: #{pass}\nFail cases: #{fail}\nTotal needles found: #{found}\n\n"
+
+  # Remove our Needle from the search string and verify all failures
+  alphabet.pop
+  pass = 0
+  fail = 0
+  found = 0
+
+  10_000.times do
+    txt = ''
+    # make a random string of 100 characters
+    100.times do
+      num = rand(alphabet.length)
+      txt << alphabet[num]
+    end
+
+    brute_force = brute_force_search.search(txt)
+    kmp = kmp_search.search(txt)
+    boyer_moore = boyer_moore_search.search(txt)
+
+    if brute_force == kmp && brute_force == boyer_moore
+      pass += 1
+    else
+      fail += 1
+    end
+
+    if brute_force < txt.length
+      found += 1
+    end
+
+  end
+
+  puts "========================NO NEEDLE CASES=============================\n"
+  puts "Pass cases: #{pass}\nFail cases: #{fail}\nTotal needles found: #{found}\n\n"
+
+  # Edge case, needle is at end of search string
+  pass = 0
+  fail = 0
+  found = 0
+
+  10_000.times do
+    txt = ''
+    # make a random string of 100 characters
+    100.times do
+      num = rand(alphabet.length)
+      txt << alphabet[num]
+    end
+    txt << needle
+
+    brute_force = brute_force_search.search(txt)
+    kmp = kmp_search.search(txt)
+    boyer_moore = boyer_moore_search.search(txt)
+
+    if brute_force == kmp && brute_force == boyer_moore
+      pass += 1
+    else
+      fail += 1
+    end
+
+    if brute_force < txt.length
+      found += 1
+    end
+
+  end
+  puts "========================NEEDLE AT END CASES=============================\n"
+  puts "Pass cases: #{pass}\nFail cases: #{fail}\nTotal needles found: #{found}\n\n"
+
+  # Edge case, needle is at beginning of search string
+  pass = 0
+  fail = 0
+  found = 0
+
+  10_000.times do
+    txt = 'NEEDLE'
+    # make a random string of 100 characters
+    100.times do
+      num = rand(alphabet.length)
+      txt << alphabet[num]
+    end
+
+    brute_force = brute_force_search.search(txt)
+    kmp = kmp_search.search(txt)
+    boyer_moore = boyer_moore_search.search(txt)
+
+    if brute_force == kmp && brute_force == boyer_moore
+      pass += 1
+    else
+      fail += 1
+    end
+
+    if brute_force < txt.length
+      found += 1
+    end
+
+  end
+
+  puts "========================NEEDLE AT BEGINNING CASES=============================\n"
+  puts "Pass cases: #{pass}\nFail cases: #{fail}\nTotal needles found: #{found}\n\n"
+
 end
 
-txt << 'ABABAC'
-
-brute_force_search = SubstringSearch::BruteForce.new('ABABAC')
-puts brute_force_search.search(txt)
-
-kmp_search = SubstringSearch::KMP.new('ABABAC')
-puts kmp_search.search(txt)
-
-boyer_moore_search = SubstringSearch::BoyerMoore.new('ABABAC')
-puts boyer_moore_search.search(txt)
+correctness(alphabet)
